a32b43a533593f5284f7c4e02127af8b6b648c3c
abiiiaja <kalindraaraya@gmail.com>
2026-03-03 15:42:28 +0700

    first commit

---
+++ pemdas_segitiga_pro_fung.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+ 
+float a, t;
+
+void Input()
+{
+    cout << "Masukkan alas segitiga (dalam cm): ";
+    cin >> a;
+    cout << "Masukkan tinggi segitiga (dalam cm): ";
+    cin >> t;
+}
+
+float Luas(float n, float y){
+    return 0.5*n*y;
+}
+
+void Output(){
+    cout << "Luas segitiga: " << Luas(a, t) << " cm^2" << endl;  
+}
+
+int main(){
+    Input();
+    Output();
+}
